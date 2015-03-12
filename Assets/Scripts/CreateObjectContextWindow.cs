e4cc87fc5db5f33dfde01f046fe3d3db2cd775c2
David Bethancourt <davidbethancourt@gmail.com>
2015-03-12 14:40:08 -0400

    framework, basic animation and prefabs, working menu system

---
+++ Assets/Scripts/CreateObjectContextWindow.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateObjectContextWindow : MonoBehaviour 
+{
+    public GameObject Pog;
+    private bool _contextMenuOpen = false;
+    private Vector2 _rightClickScreenPosition;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetMouseButton(1))
+        {
+            if (!_contextMenuOpen)
+            {
+                _rightClickScreenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                _contextMenuOpen = true;
+            }
+        }
+	}
+
+    private float sliderValue = 1.0f;
+    private float maxSliderValue = 10.0f;
+
+    void OnGUI()
+    {
+        if (_contextMenuOpen)
+        {
+            GUILayout.BeginArea(new Rect(_rightClickScreenPosition.x, Screen.height - _rightClickScreenPosition.y, 200, 200));
+                GUILayout.BeginVertical("box");
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Create Object");
+                    if (GUILayout.Button("X", GUILayout.Width(23.0f)))
+                    {
+                        _contextMenuOpen = false;
+                    }
+                    GUILayout.EndHorizontal();
+                if (GUILayout.Button("Pog"))
+                {
+                    _contextMenuOpen = false;
+                    Vector3 worldpoint = Camera.main.ScreenToWorldPoint(new Vector3(_rightClickScreenPosition.x, _rightClickScreenPosition.y, 0));
+                    Instantiate(Pog, new Vector3(worldpoint.x, worldpoint.y, 0), Quaternion.identity);
+                }
+                GUILayout.EndVertical();
+            GUILayout.EndArea();
+        }
+    }
+
+    void DrawWindow(int windowId)
+    {
+        GUILayout.Label("this is a label");
+        if (GUILayout.Button("Close Window"))
+        {
+            _contextMenuOpen = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PogMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PogMovement : MonoBehaviour {
+
+    public Vector2 WalkSpeed = new Vector2(1, 1);
+    private Vector2 _intendedMovement;
+    private Animator _pogAnimator;
+    private float _initialRotateQuickness = 1.0f; // bigger is quicker
+    private GameObject _turkeyLeg;
+    float _angle = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        _pogAnimator = GetComponent<Animator>();
+        _turkeyLeg = GameObject.FindGameObjectWithTag("TurkeyLeg");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 originalPosition = this.transform.position;
+        Vector3 desiredPosition = Vector3.MoveTowards(this.transform.position, _turkeyLeg.transform.position, 1f);
+        Vector3 moveDirection = desiredPosition - originalPosition;
+
+        _intendedMovement = new Vector2(WalkSpeed.x * moveDirection.x, WalkSpeed.y * moveDirection.y);
+        if ( Math.Abs(_intendedMovement.x) > 0.2f || Math.Abs(_intendedMovement.y) > 0.2f) 
+        {
+            _pogAnimator.SetBool("Walking", true);
+        }
+        else
+        {
+            _pogAnimator.SetBool("Walking", false);
+        }
+
+        if (moveDirection != Vector3.zero)
+        {
+            _angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg + 90; //+90 is because the pog default is facing down
+            Quaternion properRotation = Quaternion.AngleAxis(_angle, Vector3.forward);
+            if (transform.rotation != properRotation)
+            {
+                Quaternion current = transform.localRotation;
+                transform.rotation = Quaternion.Slerp(current, properRotation, Time.deltaTime * _initialRotateQuickness);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Apply movement to the rigidbody
+        rigidbody2D.velocity = _intendedMovement;
+        rigidbody2D.rotation = _angle;
+    }
+}
